2b66a6ce5bce147a685191b5689a25ee74497e77
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-09-25 10:47:10 +0900

    main.cppと各動作確認コードでコンパイル成功

--- src/check_rotary_actuator.cpp
+++ src/check_rotary_actuator.cpp
@@ -9,11 +9,10 @@ PCA9685 pwmPCA9685(0x40);
 RotaryActuator BaseRotateUnit{
   PCAMotor(pwmPCA9685, Channels::dc_base_rotate),
   ESP32Encoder(),
-  PID(Gains::pid_base_rotate, Gains::default_integral_limit),
+  PID<RadianAbsPi<float>>(Gains::pid_base_rotate, Gains::default_integral_limit),
   Pins::en_base_rotate,
   Configs::pulse2ang_base_rotate
 };
-
 
 int makeAngleStep(){
   static int angle = 0;
